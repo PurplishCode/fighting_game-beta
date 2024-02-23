ce173b046b887360b95d0819e38343ab5c890781
zamm08 <ahmadmemedhh@gmail.com>
2024-02-23 14:21:54 +0700

    Create Player and Enemy

--- index.js
+++ index.js
@@ -5,4 +5,70 @@ const c = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
-BeforeUnloadEvent
+c.fillRect(0, 0, canvas.width, canvas.height);
+const gravity = 0.2;
+
+//Create Player and Enemy
+
+class Sprite{
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0
+        }else{
+            this.velocity.y += gravity
+        }
+    }
+}
+
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 10
+    }
+})
+
+player.draw ()
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+enemy.draw ()
+
+console.log(player)
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
